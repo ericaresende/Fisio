6e86bd2510a2deb680e7c50866b9cc34f90597cf
ericaresende <134442346+ericaresende@users.noreply.github.com>
2023-05-23 16:53:15 -0300

    Consultas.java

--- Consultas.java
+++ Consultas.java
@@ -1,7 +1,10 @@
 package br.com.ericacorreia.fisio.model;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class Consultas {
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dataDaConsulta;
     private Paciente paciente;
     
